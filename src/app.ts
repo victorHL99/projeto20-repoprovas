17d5ad8de749b5813cba92a96464ab83694fb73e
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-13 14:24:55 -0300

    :sparkles: feat: create index for router

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,13 @@ import 'express-async-errors'
 import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware.js'
 
 // import routes
-// import indexRouter from './routes/index.js'
+import indexRouter from './routes/index.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-// app.use(indexRouter)
+app.use(indexRouter)
 app.use(errorHandlerMiddleware)
 
 export default app
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const indexRouter = Router();
+
+export default indexRouter;
